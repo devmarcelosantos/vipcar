ffc360217470f77e8c0ab6e0ea654e903034647a
colomeramonica <colomeramonica@gmail.com>
2017-05-31 20:58:20 -0300

    Adiciona view de listagem de clientes

---
+++ resources/views/client/list_client.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.application')
+
+@section('content')
+<h1 class="ls-title-intro ls-ico-list">Listar Clientes</h1>
+<div class="ls-box">
+  <h5 class="ls-title-5">Procurar Cliente Específico:</h5>
+  <form method="post" action="{{ route('client.list') }}" class="ls-form ls-form-horizontal row">
+    <input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
+    <fieldset>
+      <label class="ls-label col-md-4 col-xs-12">
+        <b class="ls-label-text">Nome:</b>
+        <input type="text" name="name" placeholder="Nome do cliente" class="ls-field ls-no-spin">
+      </label>
+      <label class="ls-label col-md-4 col-xs-12">
+        <b class="ls-label-text">CPF:</b>
+        <input type="text" name="cpf" maxlength="11" placeholder="Cpf do cliente" class="ls-field">
+      </label>
+    </fieldset>
+    <div class="ls-actions-btn">
+      <input type="submit" value="Buscar" class="ls-btn" title="Buscar">
+      <input class="ls-btn-danger" type="reset" value="Limpar">
+    </div>
+  </form>
+
+  <table class="ls-table ls-table-striped ls-bg-header">
+    <thead>
+      <tr>
+        <th>Nome</th>
+        <th class="hidden-xs ls-data-ascending">
+          <a href="">Email</a>
+        </th>
+        <th>Endereço</th>
+        <th class="hidden-xs">Telefone</th>
+        <th>Ação</th>
+      </tr>
+    </thead>
+    <tbody>
+    @foreach ($clients as $client)
+      <tr>
+        <td><a href="" title="">{{ $client->name }}</a></td>
+        <td class="hidden-xs">{{ $client->email }}</td>
+        <td>{{ $client->address }}</td>
+        <td class="hidden-xs">{{ $client->phone }}</td>
+        <td class="ls-regroup ">
+          <div data-ls-module="dropdown" class="ls-dropdown ">
+            <a href="#" class="ls-btn ls-btn-sm ">Administrar</a>
+            <ul class="ls-dropdown-nav">
+              <li><a href="{{ route('client.editPage', $client->id) }}">Alterar</a></li>
+            </ul>
+          </div>
+        </td>
+      </tr>
+    @endforeach
+    </tbody>
+  </table>
+</div>
+@stop
