1b2927c6c6849f60b53eb70205ac4844486baacb
Marcelo Santos <marcelossantos@univem.edu.br>
2017-06-01 18:29:45 -0300

    Altera lógica de busca do campo procurar cliente

--- app/Client.php
+++ app/Client.php
@@ -2,24 +2,23 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use \App\Client;
 
 class Client extends Model{
   protected $fillable = ['name', 'cpf'];
 
   public function getClient($field)
   {
-    if(is_null($field['name']) && is_null($field['cpf']))
-    {
-    } elseif (!is_null($field['name']) || !is_null($field['cpf'])) {
-      $client = \App\Client::where('name', 'LIKE', '%'.$field['name'].'%')
+    if(!is_null($field['name']) || !is_null($field['cpf'])) {
+      $client = Client::where('name', 'LIKE', '%'.$field['name'].'%')
                          ->orWhere('cpf', $field["cpf"])->get();
     }
-    return $client;    
+    return $client;
   }
 
   public function getClients()
   {
-    $clients = \App\Client::all();
+    $clients = Client::all();
     return $clients;
   }
 
