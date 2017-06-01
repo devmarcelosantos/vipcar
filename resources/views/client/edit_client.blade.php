1d09f67a4068235d12d3a47d7350b59b81eb2d21
Marcelo Santos <marcelossantos@univem.edu.br>
2017-06-01 18:36:32 -0300

    Altera form da tela de editar cliente

--- resources/views/client/edit_client.blade.php
+++ resources/views/client/edit_client.blade.php
@@ -3,35 +3,35 @@
 @section('content')
 <h1 class="ls-title-intro ls-ico-list">Editar Cliente</h1>
 <div class="ls-box">
-  <form method="post" action="{{route('client.edit',$client->id)}}" class="ls-form row">
+  <form method="POST" action="{{ route('client.edit', $client->id) }}" class="ls-form row">
     <input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
     <fieldset>
       <label class="ls-label col-md-3">
         <b class="ls-label-text">Nome</b>
-        <input value="{{ $client->name }}" type="text" data-ls-module="charCounter" maxlength="100" name="name" placeholder="Nome do Produto" required >
+        <input value="{{ $client->name }}" type="text" data-ls-module="charCounter" maxlength="50" name="name" placeholder="Nome do Produto" required >
       </label>
        <label class="ls-label col-md-2">
         <b class="ls-label-text">CPF</b>
-        <input value="{{ $client->cpf }}" class="ls-no-spin" type="number" name="cpf" placeholder="Endereço" required>
+        <input value="{{ $client->cpf }}" class="ls-no-spin" maxlength="11" type="text" name="cpf" placeholder="Cpf" required>
       </label>
        <label class="ls-label col-md-2">
         <b class="ls-label-text">Email</b>
-        <input value="{{ $client->email }}" class="ls-no-spin" type="text" name="email" placeholder="Endereço" required>
+        <input value="{{ $client->email }}" class="ls-no-spin" maxlength="80" type="text" name="email" placeholder="Email" required>
       </label>
       <label class="ls-label col-md-2">
         <b class="ls-label-text">Endereco</b>
-        <input value="{{ $client->address }}" class="ls-no-spin" type="text" name="address" placeholder="Endereço" required>
+        <input value="{{ $client->address }}" class="ls-no-spin" maxlength="80" type="text" name="address" placeholder="Endereço" required>
       </label>
        <label class="ls-label col-md-2">
         <b class="ls-label-text">Telefone</b>
-        <input value="{{ $client->phone }}" class="ls-no-spin" type="number" name="phone" placeholder="Endereço" required>
+        <input value="{{ $client->phone }}" class="ls-no-spin" maxlength="11" type="text" name="phone" placeholder="Telefone" required>
       </label>
        <label class="ls-label col-md-2">
         <b class="ls-label-text">Data de Nascimento</b>
-        <input value="{{ $client->birth_date }}" class="ls-no-spin" type="number" name="birth_date" placeholder="Endereço" required>
+        <input value="{{ $client->birth_date }}" class="ls-no-spin" type="date" name="birth_date" placeholder="00/00/0000" required>
       </label>
     </fieldset>
-    <div class="ls-actions-btn">
+    <div class="ls-actions-btn ls-no-padding-bottom">
       <input type="submit" value="Atualizar" class="ls-btn" title="update">
     </div>
   </form>
