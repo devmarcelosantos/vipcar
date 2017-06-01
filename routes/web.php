afd0c5d75e9ae631da7aaa3e1a5fb71eae95a8ba
Marcelo Santos <marcelossantos@univem.edu.br>
2017-06-01 18:31:25 -0300

    Ajusta rotas para todas as ações de cliente (criar, listar_ editar, excluir)

--- routes/web.php
+++ routes/web.php
@@ -21,17 +21,30 @@ Route::get('/logout', 'Auth\LoginController@logout');
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
 
-//================== ROTAS PARA CLIENTE ==================//
+//================================== ROTAS PARA CLIENTE ==================================//
 // ROTAS PARA ADICIONAR CLIENTES
 Route::get('/client/add', 'ClientController@get_add_client')->name('client.add');
-Route::post('/client/add', 'ClientController@post_add_client')->name('client.add');
+Route::post('/client/add', 'ClientController@post_add_client')->name('client.postAdd');
 
 // ROTAS PARA LISTAR CLIENTES
-Route::get('/client', 'ClientController@get_list_client')->name('client.listPage');
+Route::get('/client', function() { return redirect()->route('clients'); });
 Route::post('/client', 'ClientController@post_list_client')->name('client.list');
 Route::get('/clients', 'ClientController@list_clients')->name('clients');
 
 // ROTAS PARA EDITAR CLIENTE NO BANCO DE DADOS
-Route::get('/client/edit/{id}', ['uses' => 'ClientController@get_edit_client', 'as' => 'client.editPage']);
-Route::post('/client/edit/{id}', ['uses' => 'ClientController@post_edit_client', 'as' => 'client.edit']);
-Route::get('/client/delete/{id}', ['uses' => 'ClientController@delete_client', 'as' => 'client.delete']);
+Route::get('/client/edit/{id}', 'ClientController@get_edit_client')->name('client.edit');
+Route::post('/client/edit/{id}', 'ClientController@post_edit_client')->name('client.postEdit');
+Route::get('/client/delete/{id}', 'ClientController@delete_client')->name('client.delete');
+
+//========================================================================================//
+
+//================================== ROTAS PARA VEÍCULO ==================================//
+// ROTAS PARA ACICIONAR VEICULO
+
+// ROTAS PARA LISTAR VEICULO
+
+// ROTAS PARA EDITAR VEICULO
+
+// ROTAS PARA DELETAR VEICULO
+
+//========================================================================================//
