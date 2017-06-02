5d9b4ea7a55d39ae963e85c36c2e019e4cd19b3e
Marcelo Santos <marcelossantos@univem.edu.br>
2017-06-01 21:47:56 -0300

    Controller para Vehicle

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -1,10 +1,37 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use DB;
+use \App\Vehicle;
+use \App\Client;
 use Illuminate\Http\Request;
+use App\Http\Requests;
 
 class VehicleController extends Controller
 {
-    //
+	private $vehicle;
+	private $client;
+
+	public function __construct(Vehicle $vehicle, Client $client)
+  {
+    $this->vehicle = $vehicle;
+    $this->vechicles = $vehicle;
+
+		$this->client = $client;
+    $this->middleware('auth');
+  }
+
+  public function get_add_vehicle(Request $field)
+  {
+		if(!is_null($field['name']) || !is_null($field['cpf'])) {
+      $clients = $this->client->getClient($field);
+    }
+  	return view('vehicle/add_vehicle', compact('clients'));
+  }
+
+  public function post_add_vehicle(Request $info)
+  {
+    $vehicle = $this->vehicle->addVehicle($info);
+    return redirect()->route('vehicles');
+  }
 }
