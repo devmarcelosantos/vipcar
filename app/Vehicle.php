4e509cbbbd3da4633066e28cd3aa95a7fb1344e8
Marcelo Santos <marcelossantos@univem.edu.br>
2017-06-01 21:45:55 -0300

    Model para adição de veículo no banco

--- app/Vehicle.php
+++ app/Vehicle.php
@@ -3,8 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use \App\Vehicle;
 
 class Vehicle extends Model
 {
-    
+
+	public function addVehicle($field)
+  {
+    $vehicle = new Vehicle();
+    $vehicle->brand = $field['brand'];
+    $vehicle->model = $field['model'];
+    $vehicle->year = $field['year'];
+    $vehicle->client_id = $field['codCliente'];
+    $vehicle->save();
+  }
 }
